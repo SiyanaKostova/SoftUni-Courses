9a711bb873a22896d3ba7953fad3f6f353a545e2
SiyanaKostova <siyana.kostova06@abv.bg>
2022-07-05 12:40:49 +0300

    SoftUni-C# fundamentals
    Added folder and files for Objects and Classes - Lab.

---
+++ softuni/c#fund/13. Objects and Classes - Lab/04. Students/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _04._Students
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Student> students = new List<Student>();
+
+            string command = Console.ReadLine();
+
+            while (command != "end")
+            {
+                string[] tokens = command.Split();
+
+                string firstName = tokens[0];
+                string lastName = tokens[1];
+                int age = int.Parse(tokens[2]);
+                string city = tokens[3];
+
+                Student student = new Student()
+                {
+                    FirstName = firstName,
+                    LastName = lastName,
+                    Age = age,
+                    City = city
+                };
+
+                students.Add(student);
+                command = Console.ReadLine();
+            }
+
+            string filterCity = Console.ReadLine();
+
+            foreach (Student student in students)
+            {
+                if (student.City == filterCity)
+                {
+                    Console.WriteLine($"{student.FirstName} {student.LastName} is {student.Age} years old.");
+                }
+            }
+        }
+    }
+
+    class Student
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int Age { get; set; }
+        public string City { get; set; }
+    }
+}
